3f116a703036ce63c83aa13655b742badf33be86
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2018-02-27 16:14:03 +0100

    Moved the old SmallZarembaModel to model_old.py.

--- pytorch_lm/model.py
+++ pytorch_lm/model.py
@@ -1,62 +1,6 @@
-import torch
 import torch.nn as nn
 
 from pytorch_lm.lstm import Lstm
-
-
-class SmallZarembaModel(nn.Module):
-    """"Implements the small model from Zaremba (2014)."""
-    def __init__(self, vocab_size):
-        super(SmallZarembaModel, self).__init__()
-        self.hidden_size = 200
-        self.input_size = 200
-        self.num_layers = 2
-
-        self.encoder = nn.Embedding(vocab_size, self.hidden_size)
-        self.rnn = Lstm(self.input_size, self.hidden_size, self.num_layers)
-        self.decoder = nn.Linear(self.hidden_size, vocab_size)
-        self.init_weights()
-
-    def init_weights(self):
-        initrange = 0.1
-        self.encoder.weight.data.uniform_(-initrange, initrange)
-        self.decoder.bias.data.uniform_(-initrange, initrange)  # fill_(0)
-        self.decoder.weight.data.uniform_(-initrange, initrange)
-
-    def forward(self, input, hidden):
-        emb = self.encoder(input)
-        # self.rnn.flatten_parameters()
-        output, hidden = self.rnn(emb, hidden)
-        decoded = self.decoder(
-            output.view(output.size(0) * output.size(1), output.size(2)))
-        return decoded.view(output.size(0), output.size(1), decoded.size(1)), hidden
-
-    def init_hidden(self, batch_size):
-        return self.rnn.init_hidden(batch_size)
-
-    def save_parameters(self, out_dict=None, prefix=''):
-        if out_dict is None:
-            out_dict = {}
-        self.rnn.save_parameters(out_dict, prefix=prefix + 'RNN/')
-        out_dict[prefix + 'embedding'] = self.encoder.weight.data.cpu().numpy()
-        # .T is required because stupid Linear stores the weights transposed
-        out_dict[prefix + 'softmax_w'] = self.decoder.weight.data.cpu().numpy().T
-        out_dict[prefix + 'softmax_b'] = self.decoder.bias.data.cpu().numpy()
-        return out_dict
-
-    def load_parameters(self, data_dict, prefix=''):
-        def set_data(parameter, value, is_cuda):
-            t = torch.from_numpy(value)
-            if is_cuda:
-                t = t.cuda()
-            parameter.data = t
-
-        is_cuda = self.encoder.weight.is_cuda
-        self.rnn.load_parameters(data_dict, prefix=prefix + 'RNN/')
-        set_data(self.encoder.weight, data_dict[prefix + 'embedding'], is_cuda)
-        # .T is required because stupid Linear stores the weights transposed
-        set_data(self.decoder.weight, data_dict[prefix + 'softmax_w'].T, is_cuda)
-        set_data(self.decoder.bias, data_dict[prefix + 'softmax_b'], is_cuda)
 
 
 class CustomZarembaModel(nn.Module):
@@ -93,6 +37,6 @@ class CustomZarembaModel(nn.Module):
         return self.rnn.init_hidden(batch_size)
 
 
-class SmallZarembaModel2(CustomZarembaModel):
+class SmallZarembaModel(CustomZarembaModel):
     def __init__(self, vocab_size):
-        super(SmallZarembaModel2, self).__init__(vocab_size, 200, 2, 0.1, 0)
+        super(SmallZarembaModel, self).__init__(vocab_size, 200, 2, 0.1, 0)
