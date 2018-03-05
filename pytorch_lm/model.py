34e9acaff478f4cb75763270e455882710d26fc1
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2018-03-05 09:20:12 +0100

    Added the Press&Wolf models.

--- pytorch_lm/model.py
+++ pytorch_lm/model.py
@@ -6,7 +6,7 @@ from pytorch_lm.lstm import Lstm
 class CustomZarembaModel(nn.Module):
     """Implements a generic embedding - LSTM - softmax LM."""
     def __init__(self, vocab_size, hidden_size=200, num_layers=2,
-                 weight_scale=0.1, dropout=0.5):
+                 weight_scale=0.1, dropout=0.5, init_weights=True):
         super(CustomZarembaModel, self).__init__()
         self.vocab_size = vocab_size
         self.hidden_size = hidden_size
@@ -17,7 +17,8 @@ class CustomZarembaModel(nn.Module):
         self.encoder = nn.Embedding(vocab_size, hidden_size)
         self.rnn = Lstm(hidden_size, hidden_size, num_layers, dropout)
         self.decoder = nn.Linear(hidden_size, vocab_size)
-        self.init_weights(weight_scale)
+        if init_weights:
+            self.init_weights(weight_scale)
 
     def init_weights(self, initrange):
         """Weight are initialized to between - and + initrange uniformly."""
@@ -50,3 +51,37 @@ class MediumZarembaModel(CustomZarembaModel):
 class LargeZarembaModel(CustomZarembaModel):
     def __init__(self, vocab_size):
         super(LargeZarembaModel, self).__init__(vocab_size, 1500, 2, 0.04, 0.65)
+
+
+class PressAndWolfModel(CustomZarembaModel):
+    """
+    Optionally tie weights as in:
+    "Using the Output Embedding to Improve Language Models" (Press & Wolf 2016)
+    https://arxiv.org/abs/1608.05859
+    and
+    "Tying Word Vectors and Word Classifiers: A Loss Framework for Language Modeling" (Inan et al. 2016)
+    https://arxiv.org/abs/1611.01462
+    """
+    def __init__(self, vocab_size, hidden_size=200, num_layers=2,
+                 weight_scale=0.1, dropout=0.5):
+        super(CustomZarembaModel).__init__(
+            vocab_size, hidden_size, num_layers, weight_scale, dropout,
+            init_weights=False)
+        # Linear.weight is transposed, so this will just work
+        self.decoder.weight = self.encoder.weight
+        self.init_weights()
+
+
+class SmallPressAndWolfModel(CustomZarembaModel):
+    def __init__(self, vocab_size):
+        super(SmallPressAndWolfModel, self).__init__(vocab_size, 200, 2, 0.1, 0)
+
+
+class MediumPressAndWolfModel(CustomZarembaModel):
+    def __init__(self, vocab_size):
+        super(MediumPressAndWolfModel, self).__init__(vocab_size, 650, 2, 0.05, 0.5)
+
+
+class LargePressAndWolfModel(CustomZarembaModel):
+    def __init__(self, vocab_size):
+        super(LargePressAndWolfModel, self).__init__(vocab_size, 1500, 2, 0.04, 0.65)
