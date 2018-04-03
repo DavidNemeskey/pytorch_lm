eae0a256f1ea3974a41d28c13265c021e1b4f770
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2018-04-03 18:43:53 +0200

    Moved the RNN subclasses to pytorch_lm/rnn.

---
+++ pytorch_lm/rnn/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8 :
+
+"""Package for all the different RNN architectures."""
+
+from .lstm import Lstm, ZarembaLstmCell, MoonLstmCell, TiedGalLstmCell
+from .lstm import UntiedGalLstmCell, SemeniutaLstmCell
+from .rhn import Rhn
+
+__all__ = [Lstm, ZarembaLstmCell, MoonLstmCell, TiedGalLstmCell,
+           UntiedGalLstmCell, SemeniutaLstmCell, Rhn]
